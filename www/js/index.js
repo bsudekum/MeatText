ab5fe3e4ae757fb67ef1de808fd20c35aae4f0f5
Bobby Sudekum <rsudekum@gmail.com>
2012-10-18 00:26:02 -0400

    tons of bug fixes

--- www/js/index.js
+++ www/js/index.js
@@ -1,24 +1,40 @@
+
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
-    navigator.splashscreen.hide()
 
 
-    var map = L.map('map')
+    map = L.map('map')
         .setView([37.76718664006672, -122.42511749267578], 13);
 
-    var tiles = L.tileLayer('http://a.tiles.mapbox.com/v3/bobbysud.map-tyt3admo/{z}/{x}/{y}.png', {
+    tiles = L.tileLayer('http://a.tiles.mapbox.com/v3/bobbysud.map-tyt3admo/{z}/{x}/{y}.png', {
         zoomControl: false,
         detectRetina: true,
         maxZoom: 17,
     })
         .addTo(map);
 
+    function onLocationFoundStart(e) {
+        }
+
+    function onLocationErrorStart(e) {
+            navigator.notification.alert("Please make sure your location services area enabled.");
+        }
+
+    map.on('locationfound', onLocationFoundStart);
+    map.on('locationerror', onLocationErrorStart);
+
+        map.locate({
+            setView: true,
+            maxZoom: 15,
+            enableHighAccuracy: true,
+        });
+
 
     function onMapClick(e) {
 
-        var lat = e.latlng.lat.toFixed(5);
-        var lng = e.latlng.lng.toFixed(5);
+        lat = e.latlng.lat.toFixed(5);
+        lng = e.latlng.lng.toFixed(5);
         latlng = +lat + ',' + lng;
 
         url = " http://maps.apple.com/maps?q="+latlng + "&ll=" + latlng+"&z=18";
@@ -28,10 +44,7 @@ function onDeviceReady() {
             .addTo(map)
             .bindPopup("<a href='#two'/><h3 id='sent' style='color:black;text-decoration:none;'>Text This Location ►</h3></a>")
             .openPopup()
-
             elem = document.getElementById('sentbox').innerHTML = "<h3 style='text-align:center;font-color:#00CC33;'></h3>";
-
-  
     };
 
     map.on('click', onMapClick);
@@ -43,13 +56,18 @@ function onDeviceReady() {
 
         function onLocationFound(e) {
 
+        lat = e.latlng.lat.toFixed(5);
+        lng = e.latlng.lng.toFixed(5);
+        latlng = +lat + ',' + lng;
+
+        url = " http://maps.apple.com/maps?q="+latlng + "&ll=" + latlng+"&z=18";
+        message = $(url).html();
             L.marker(e.latlng, {draggable: true})
-            .addTo(map).bindPopup("<a href='#two'/><h3 id='sent' style='color:black;text-decoration:none;'>Text This Location ►</h3></a>")
+            .addTo(map).bindPopup("<a href='#two'/><h3 id='sent' style='color:black;text-decoration:none;'>Text Your Location ►</h3></a>")
             .openPopup()
         }
 
         function onLocationError(e) {
-            alert("Please make sure your location services are enabled.");
         }
 
         map.on('locationfound', onLocationFound);
@@ -72,18 +90,19 @@ function onDeviceReady() {
 
 //SMS
 var ComposeSMS = function () {
-    contents = $("#name").val();
+    contents = $("input#textValue").val();
     messageTwo = contents + url;
-    console.log(messageTwo);
     window.plugins.smsComposer.showSMSComposerWithCB(myCallback,'', messageTwo);
+
+    console.log(messageTwo);
 }
 
 var myCallback = function(result){
     if(result == 1)
-
         elem = document.getElementById('sentbox').innerHTML = "<h3 style='text-align:center;'>Location Sent!</h3>";
-
         elem = document.getElementById('sent').innerHTML = "<a href='#two'/><h3 id='sent' style='color:black;text-decoration:none;text-align:center;margin:5px'>Location Sent!</h3></a>";
+    
+    
 };
 
 
