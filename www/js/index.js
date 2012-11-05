2ed7080a33554da0f584879fb86b48164af1660e
Bobby Sudekum <rsudekum@gmail.com>
2012-11-04 22:33:38 -0800

    Added URL shorten

--- www/js/index.js
+++ www/js/index.js
@@ -12,7 +12,7 @@ map = new L.Map('map', {layers: [locationClick, locationPerson]});
 
 L.Browser.retina = true;
 
-  L.zoomTMSLayer({
+  L.zoomTMSLayer({ 
     url:            'http://{s}.tiles.mapbox.com/v3/bobbysud.map-tyt3admo/',
     layername :     '',
     serviceVersion: '',
@@ -28,8 +28,8 @@ L.Browser.retina = true;
 
     function onLocationFound(e) {
 
-        lat = e.latlng.lat.toFixed(5);
-        lng = e.latlng.lng.toFixed(5);
+        lat = e.latlng.lat.toFixed(7);
+        lng = e.latlng.lng.toFixed(7);
         latlng = +lat + ',' + lng;
         url = "http://maps.apple.com/maps?q="+latlng;
         message = $(url)
@@ -57,8 +57,8 @@ L.Browser.retina = true;
 
     function onMapClick(e) {
 
-        lat = e.latlng.lat.toFixed(5);
-        lng = e.latlng.lng.toFixed(5);
+        lat = e.latlng.lat.toFixed(7);
+        lng = e.latlng.lng.toFixed(7);
         latlng =lat+ ',' + lng;
         url = "http://maps.apple.com/maps?q="+latlng;
         message = $(url)
@@ -79,10 +79,10 @@ L.Browser.retina = true;
 
         function onLocationFound(e) {
 
-        lat = e.latlng.lat.toFixed(5);
-        lng = e.latlng.lng.toFixed(5);
+        lat = e.latlng.lat.toFixed(7);
+        lng = e.latlng.lng.toFixed(7);
         latlng = +lat + ',' + lng;
-        url = "http://maps.apple.com/maps?spn="+latlng;
+        url = "http://maps.apple.com/maps?q="+latlng;
         message = $(url)
         markerLocation = new L.LatLng(e.latlng.lat, e.latlng.lng);
         marker = new L.Marker(markerLocation, {draggable:true});
@@ -110,24 +110,74 @@ L.Browser.retina = true;
      var bingGeocoder = new L.Control.BingGeocoder('AmFJ03ozVugKu0Y_uijzwvFEKfKY5VCesm1eiBqGhchxQ3uKFUQMYsKJLNdfHsIR');
         map.addControl(bingGeocoder);
 
-
-
-
+    
 }; //Device on onDeviceReady
 
 
 //SMS for onclick event
 var ComposeSMS = function () {
-    messageTwo = "Let's meet here.\n"+url;
+    function get_short_url(url, login, api_key, func)
+    {
+        $.getJSON(
+            "http://api.bitly.com/v3/shorten?callback=?", 
+            { 
+                "format": "json",
+                "apiKey": api_key,
+                "login": login,
+                "longUrl": url
+            },
+            function(response)
+            {
+                func(response.data.url);
+            }
+        );
+    }
+
+
+    var login = "o_7iuqro3rja";
+    var api_key = "R_0e5b4318f72e53dfee13fb1491229204";
+
+    get_short_url(url, login, api_key, function(short_url) {
+        console.log(short_url);
+    
+
+    messageTwo = "Let's meet here.\n\n"+short_url;
     window.plugins.smsComposer.showSMSComposerWithCB(myCallback,'', messageTwo);
     console.log(messageTwo);
+    });
+
 }
 
 //SMS for your location
 var ComposeSMSme = function () {
-    messageTwo = "I'm right here.\n"+url;
+    function get_short_url(url, login, api_key, func)
+    {
+        $.getJSON(
+            "http://api.bitly.com/v3/shorten?callback=?", 
+            { 
+                "format": "json",
+                "apiKey": api_key,
+                "login": login,
+                "longUrl": url
+            },
+            function(response)
+            {
+                func(response.data.url);
+            }
+        );
+    }
+
+    var login = "o_7iuqro3rja";
+    var api_key = "R_0e5b4318f72e53dfee13fb1491229204";
+
+    get_short_url(url, login, api_key, function(short_url) {
+        console.log(short_url);
+    
+    messageTwo = "I'm right here.\n\n"+short_url;
     window.plugins.smsComposer.showSMSComposerWithCB(myCallback,'', messageTwo);
     console.log(messageTwo);
+    });
+
 }
 
 var myCallback = function(result){
