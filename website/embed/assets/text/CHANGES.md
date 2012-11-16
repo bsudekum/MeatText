8417824ba089ae128623971b0c1abf67523e7620
Bobby Sudekum <rsudekum@gmail.com>
2012-11-15 17:46:12 -0800

    added website back into repo

---
+++ website/embed/assets/text/CHANGES.md
@@ -0,0 +1,105 @@
+twilio-php Changelog
+====================
+
+Version 3.7.0
+-------------
+
+Released on August 6, 2012
+
+- Add retry support for idempotent HTTP requests that result in a 500 server
+  error (default is 1 attempt, however this can be configured).
+- Throw a Services_Twilio_RestException instead of a DomainException if the
+  response content cannot be parsed as JSON (usually indicates a 500 error)
+
+Version 3.6.0
+-------------
+
+Released on August 5, 2012
+
+- Add support for Queues and Members. Includes tests and documentation for the
+  new functionality.
+
+Version 3.5.2
+-------------
+
+Released on July 23, 2012
+
+- Fix an issue introduced in the 3.5.0 release where updating or muting
+  a participant would [throw an exception instead of muting the
+  participant][mute-request]. 
+  (Reporter: [Alex Chan](/alexcchan))
+
+- Fix an issue introduced in the 3.5.0 release where [filtering an iterator
+with parameters would not work properly][paging-request] on subsequent HTTP
+requests. (Reporters: [Alex Chan](/alexcchan), Ivor O'Connor)
+
+[mute-request]: /twilio/twilio-php/pull/74
+[paging-request]: /twilio/twilio-php/pull/75
+
+Version 3.5.1
+-------------
+
+Released on July 2, 2012
+
+- Fix an issue introduced in the 3.5.0 release that would cause a second HTTP
+request for an instance resource [to request an incorrect URI][issue-71].
+
+[issue-71]: https://github.com/twilio/twilio-php/pull/71
+
+Version 3.5.0
+-------------
+
+Released on June 30, 2012
+
+- Support paging through resources using the `next_page_uri` parameter instead
+of manually constructing parameters using the `Page` and `PageSize` parameters.
+Specifically, this allows the library to use the `AfterSid` parameter, which
+leads to improved performance when paging deep into your resource list.
+
+This involved a major refactor of the library. The documented interface to
+twilio-php will not change. However, some undocumented public methods are no
+longer supported. Specifically, the following classes are no longer available:
+
+- `Services/Twilio/ArrayDataProxy.php`
+- `Services/Twilio/CachingDataProxy.php`
+- `Services/Twilio/DataProxy.php`
+
+In addition, the following public methods have been removed:
+
+- `setProxy`, in `Services/Twilio/InstanceResource.php`
+- `getSchema`, in `Services/Twilio/ListResource.php`,
+    `Services/Twilio/Rest/AvailablePhoneNumbers.php`,
+    `Services/Twilio/Rest/SMSMessages.php`
+
+- `retrieveData`, in `Services/Twilio/Resource.php`
+- `deleteData`, in `Services/Twilio/Resource.php`
+- `addSubresource`, in `Services/Twilio/Resource.php`
+
+Please check your own code for compatibility before upgrading.
+
+Version 3.3.2
+-------------
+
+Released on May 3, 2012
+
+- If you pass booleans in as TwiML (ex transcribe="true"), convert them to
+  the strings "true" and "false" instead of outputting the incorrect values 
+  1 and "".
+
+Version 3.3.1
+-------------
+
+Released on May 1, 2012
+
+- Use the 'Accept-Charset' header to specify we want to receive UTF-8 encoded 
+data from the Twilio API. Remove unused XML parsing logic, as the library never 
+requests XML data.
+
+Version 3.2.4
+-------------
+
+Released on March 14, 2012
+
+- If no version is passed to the Services_Twilio constructor, the library will
+  default to the most recent API version.
+
--- website/map/map.js
+++ website/map/map.js
@@ -1,6 +1,6 @@
 function runMap() {
     
-    var map = L.map('map', {scrollWheelZoom: false});
+    var map = L.map('map', {scrollWheelZoom: true});
     var marker;
 
     marker= new L.LayerGroup();
@@ -13,6 +13,7 @@ function runMap() {
             maxZoom:        18,
             tms:            false,
             detectRetina:true,
+            attrtibution: 'Hello'
           }).addTo(map);
 
         function onLocationFound(e) {
@@ -28,14 +29,6 @@ function runMap() {
 
         map.locate({setView: true, maxZoom: 17});
 
-
-    var bingGeocoder = new L.Control.BingGeocoder('AmFJ03ozVugKu0Y_uijzwvFEKfKY5VCesm1eiBqGhchxQ3uKFUQMYsKJLNdfHsIR');
-
-    var hash = new L.Hash();
-
-    function onMapClick(e) {
-        var zoom = map.getZoom();
-        
         function get_short_url(url, login, api_key, func)
             {
             $.getJSON(
@@ -53,25 +46,62 @@ function runMap() {
             );
         }
 
-        var login = "o_7iuqro3rja";
-        var api_key = "R_0e5b4318f72e53dfee13fb1491229204";
-        
-        var latlngURL = '#' + zoom + '/' + e.latlng.lat.toFixed(3) + '/' + e.latlng.lng.toFixed(3);
-      
+    var login = "o_7iuqro3rja";
+    var api_key = "R_0e5b4318f72e53dfee13fb1491229204";
+
+    var bingGeocoder = new L.Control.BingGeocoder('AmFJ03ozVugKu0Y_uijzwvFEKfKY5VCesm1eiBqGhchxQ3uKFUQMYsKJLNdfHsIR');
+
+    var hash = new L.Hash();
+
+    function onMapClick(e) {
+
+         var marker = new L.marker(e.latlng, {draggable: true}).addTo(map) //Without this, the marker is placed after the URL call happens. 
+
         var latlngStr = "http://maps.apple.com/?q="+e.latlng.lat.toFixed(20) + ',' + e.latlng.lng.toFixed(20);
 
         get_short_url(latlngStr, login, api_key, function(short_url) {
-            console.log(short_url);
+
+            console.log(short_url); //Shortened URL
         
         latlngMessage = ""+short_url+"\n Sent via MeatText.com";
 
+        var zoom = map.getZoom();
+         
+        var latlngURL = '#' + zoom + '/' + e.latlng.lat.toFixed(3) + '/' + e.latlng.lng.toFixed(3);
+        
         var marker = new L.marker(e.latlng, {draggable: true})
-        	.addTo(map)
-        	.bindPopup("<form method='post' id=payment action='assets/text/sms.php'><fieldset><ol><li><label for=name></label><input id=name name=to type=text placeholder='To?' required autofocus></li><li><label for=name></label><input id=name name=from type=text placeholder='From?' required autofocus></li><li><label for=phone></label><input id=phone name=phone type=tel placeholder='Their Phone #?' required></li><li id=location><textarea id=location name=location rows=2 required>" + latlngMessage + "</textarea></li><li id=url><textarea id=url name=url rows=2 required>" + latlngURL + "</textarea></li><li><textarea id=notes name=notes rows=1 required>Lets meet here!</textarea></li></ol></fieldset><fieldset><button type=submit id=submit>Send</button></fieldset></form>").openPopup()
+            .addTo(map)
+            .bindPopup("<img src='../img/twitter.png").openPopup()
 
         });
 
     }
+
+
+/*
+    function onMapClick(e) {
+
+         var marker = new L.marker(e.latlng, {draggable: true}).addTo(map) //Without this, the marker is placed after the URL call happens. 
+
+        var latlngStr = "http://maps.apple.com/?q="+e.latlng.lat.toFixed(20) + ',' + e.latlng.lng.toFixed(20);
+
+        get_short_url(latlngStr, login, api_key, function(short_url) {
+
+            console.log(short_url); //Shortened URL
+        
+        latlngMessage = ""+short_url+"\n Sent via MeatText.com";
+
+        var zoom = map.getZoom();
+         
+        var latlngURL = '#' + zoom + '/' + e.latlng.lat.toFixed(3) + '/' + e.latlng.lng.toFixed(3);
+        
+        var marker = new L.marker(e.latlng, {draggable: true})
+        	.addTo(map)
+        	.bindPopup("<form method='post' id=payment action='assets/text/sms.php'><fieldset><ol><li><label for=name></label><input id=name name=to type=text placeholder='To?' required autofocus></li><li><label for=name></label><input id=name name=from type=text placeholder='From?' required autofocus></li><li><label for=phone></label><input id=phone name=phone type=tel placeholder='Their Phone #?' required></li><li id=location><textarea id=location name=location rows=2 required>" + latlngMessage + "</textarea></li><li id=url><textarea id=url name=url rows=2 required>" + latlngURL + "</textarea></li><li><textarea id=notes name=notes rows=1 required>Let's meet here!</textarea></li></ol></fieldset><fieldset><button type=submit id=submit>Send</button></fieldset></form>").openPopup()
+
+        });
+
+    }*/
 
     map.on('click', onMapClick);
 
